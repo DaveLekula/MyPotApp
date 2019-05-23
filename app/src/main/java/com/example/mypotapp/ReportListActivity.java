45f413b6b4f43f03402cdb2af518965f49645b86
Themba <jeffmakinita@gmail.com>
2019-05-23 20:17:35 +0200

    listView commit

--- app/src/main/java/com/example/mypotapp/ReportListActivity.java
+++ app/src/main/java/com/example/mypotapp/ReportListActivity.java
@@ -4,11 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.pojo.CustomListview;
 import com.example.pojo.Report;
 
 import java.util.ArrayList;
@@ -17,8 +16,8 @@ public class ReportListActivity extends AppCompatActivity {
 
     private ArrayList<Report>repots = new ArrayList<>();
 
-    String[] data = {"Pollo rally","Killer","Spider Monkey","Sting Ray","Money Bag Bro","Anything Long Enough","Dog is Hungry"};
-    String[] nums ={"1","2","3","4","5","6","7"};
+    String[] data = {"Pollo rally","Killer"};
+    String[] num ={"1","2"};
     int [] image = {R.drawable.camera,R.drawable.settings};
 
     @Override
@@ -26,9 +25,10 @@ public class ReportListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_list);
 
-        final ListAdapter  adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        //final ListAdapter  adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
         ListView theListView =(ListView) findViewById(R.id.theListViewLV);
-        theListView.setAdapter(adapter);
+        CustomListview customListview = new CustomListview(this,data,num,image);
+        theListView.setAdapter(customListview);
 
         theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
--- app/src/main/java/com/example/pojo/CustomListview.java
+++ app/src/main/java/com/example/pojo/CustomListview.java
@@ -1,9 +1,15 @@
 package com.example.pojo;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.support.annotation.Nullable;
 
 import com.example.mypotapp.R;
 
@@ -25,9 +31,44 @@ public class CustomListview extends ArrayAdapter<String> {
 
     }
 
-    @androidx.annotation.NonNull
+    @NonNull
     @Override
-    public View getView(int position, @androidx.annotation.Nullable View convertView, @androidx.annotation.NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View vi= convertView;
+        ViewHolder viewHolder = null;
+
+        if (vi==null){
+
+            LayoutInflater layoutInflater = context.getLayoutInflater();
+            vi = layoutInflater.inflate(R.layout.row_layout,null,true);
+            viewHolder = new ViewHolder(vi);
+            vi.setTag(viewHolder);
+        }
+        else{
+
+            viewHolder =(ViewHolder) vi.getTag();
+
+        }
+
+        viewHolder.imageView1.setImageResource(image[position]);
+        viewHolder.tvData.setText(data[position]);
+        viewHolder.tvDesc.setText(num[position]);
+
+        return vi;
+    }
+
+    class ViewHolder
+    {
+        TextView tvData;
+        TextView tvDesc;
+        ImageView imageView1;
+
+        ViewHolder(View view){
+            tvData =(TextView) view.findViewById(R.id.rowItemTV);
+            tvDesc =(TextView) view.findViewById(R.id.rowItemDescTv);
+            imageView1= view.findViewById(R.id.rowItemImage);
+        }
+
     }
 }
